3c2f2f0ea563934dc3ea68039225e07ac0042fc2
kysh555 <on88.sawada.ai@gmail.com>
2021-01-27 17:46:08 +0900

    worksテーブルを作成

--- db/migrate/20210127070229_create_works.rb
+++ db/migrate/20210127070229_create_works.rb
@@ -1,6 +1,9 @@
 class CreateWorks < ActiveRecord::Migration[6.0]
   def change
     create_table :works do |t|
+      t.string     :title,       null: false
+      t.text       :description, null: false
+      t.references :user,        foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_141746) do
+ActiveRecord::Schema.define(version: 2021_01_27_070229) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +25,14 @@ ActiveRecord::Schema.define(version: 2021_01_19_141746) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "works", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_works_on_user_id"
+  end
+
+  add_foreign_key "works", "users"
 end
