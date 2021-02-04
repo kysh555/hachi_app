c9020e6943f5df7f65354872e085c7fd0a4eb307
kysh555 <on88.sawada.ai@gmail.com>
2021-02-04 14:49:16 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "works#index"
-  resources :works, only: [:new, :create, :show] do
+  resources :works, only: [:new, :create, :show, :edit, :update] do
     resources :comments, only: :create
   end
 end
