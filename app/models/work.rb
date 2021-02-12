32008bb9a76b01dc73404e9f126613aa664d83ab
kysh555 <on88.sawada.ai@gmail.com>
2021-02-12 11:39:28 +0900

    workモデルのバリデーションを記述

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,11 @@ class Work < ApplicationRecord
   has_many_attached :images
   belongs_to :user
   has_many :comments
+
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :images
+  end
+
 end
