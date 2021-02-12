931f9e1a0821058b65b0ebafed966ec3c5e1ea7d
kysh555 <on88.sawada.ai@gmail.com>
2021-02-12 10:30:58 +0900

    FactoryBotの記述を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.initials(number: 6)}
     email {Faker::Internet.email}
-    password {Faker.Internet.password(min_length: 6)}
+    password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
   end
 end
