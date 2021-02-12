bf9d4e2b2b9b4d2d4dcd980ddef9c82a7de2a74f
kysh555 <on88.sawada.ai@gmail.com>
2021-02-12 11:39:47 +0900

    FactoryBotの記述を変更

--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
 
     association :user
 
-    after(:build) do |message|
-      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    after(:build) do |work|
+      work.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
     end
   end
 end
