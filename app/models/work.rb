dde8d63671f73398e0af71a035d2385a86844967
kysh555 <on88.sawada.ai@gmail.com>
2021-02-23 13:37:01 +0900

    アソシエーションを追記

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,8 @@ class Work < ApplicationRecord
   has_many_attached :images
   belongs_to :user
   has_many :comments
+  has_many :work_tags
+  has_many :tags, through: :work_tags
 
   with_options presence: true do
     validates :title
