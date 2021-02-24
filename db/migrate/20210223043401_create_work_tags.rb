0cc2047b27ac4cc5308ff2c50a0fa2e69bfabb4d
kysh555 <on88.sawada.ai@gmail.com>
2021-02-24 11:25:17 +0900

    中間テーブルの作り直し

--- db/migrate/20210223043401_create_work_tags.rb
+++ db/migrate/20210223043401_create_work_tags.rb
@@ -1,6 +1,8 @@
 class CreateWorkTags < ActiveRecord::Migration[6.0]
   def change
     create_table :work_tags do |t|
+      t.references :work
+      t.references :tag
 
       t.timestamps
     end
