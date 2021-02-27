300f8d00dab60ea59e3f6493b9e5e8d70426e276
kysh555 <on88.sawada.ai@gmail.com>
2021-02-27 15:49:56 +0900

    フォームオブジェクトの作成

--- app/models/work_tag.rb
+++ app/models/work_tag.rb
@@ -1,4 +1,19 @@
-class WorkTag < ApplicationRecord
-  belongs_to :work
-  belongs_to :tag
+class WorkTag
+  include ActiveModel::Model
+  attr_accessor :title, :description, :images, :tag_name, :user_id, :work_id, :tag_id
+
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :images
+    validates :tag_name
+  end
+
+  def save
+    work = Work.create(title: title, description: description, images: images)
+    tag = Tag.where(tag_name: tag_name).first_or_initialize
+    tag.save
+
+    WorkTagRelation.create(work_id: work.id, tag_id: tag.id)
+  end
 end
