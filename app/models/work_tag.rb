f06dfc10978d23cd40d75ae0495099a53d29ff10
kysh555 <on88.sawada.ai@gmail.com>
2021-03-01 13:14:17 +0900

    フォームオブジェクト内不必要な記述を削除

--- app/models/work_tag.rb
+++ app/models/work_tag.rb
@@ -1,6 +1,6 @@
 class WorkTag
   include ActiveModel::Model
-  attr_accessor :title, :description, :images, :tag_name, :user_id, :work_id, :tag_id
+  attr_accessor :title, :description, :images, :tag_name, :user_id
 
   with_options presence: true do
     validates :title
@@ -10,7 +10,7 @@ class WorkTag
   end
 
   def save
-    work = Work.create(title: title, description: description, images: images)
+    work = Work.create(title: title, description: description, images: images, user_id: user_id)
     tag = Tag.where(tag_name: tag_name).first_or_initialize
     tag.save
 
