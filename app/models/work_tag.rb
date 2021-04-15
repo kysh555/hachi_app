d38af2d3599f20f7b6b3195f1baece78bb76e391
kysh555 <on88.sawada.ai@gmail.com>
2021-04-16 07:59:39 +0900

    refactor: form object内、新規投稿機能の記述を修正

--- app/models/work_tag.rb
+++ app/models/work_tag.rb
@@ -1,19 +1,45 @@
 class WorkTag
   include ActiveModel::Model
-  attr_accessor :title, :description, :images, :tag_name, :user_id
+  attr_accessor :title, :description, :images, :tag_name, :user_id, :work_id, :tag_id
 
   with_options presence: true do
     validates :title
     validates :description
     validates :images
-    validates :tag_name
   end
 
+  delegate :persisted?, to: :work
+
+  def initialize(attributes = nil, work: Work.new, tag: Tag.new)
+    @work = work
+    @tag = tag
+    attributes ||= default_attributes
+    super(attributes)
+  end
+  
   def save
-    work = Work.create(title: title, description: description, images: images, user_id: user_id)
-    tag = Tag.where(tag_name: tag_name).first_or_initialize
-    tag.save
-
-    WorkTagRelation.create(work_id: work.id, tag_id: tag.id)
+      @work = Work.create(title: title, description: description, images: images, user_id: user_id)
+      @tag = Tag.where(tag_name: tag_name).first_or_initialize
+      @tag.save
+      WorkTagRelation.create(work: work, tag: tag)
   end
+  
+  def to_model
+    work
+  end
+
+  private
+
+  attr_reader :work, :tag
+    
+  def default_attributes
+    {
+      title: work.title,
+      description: work.description,
+      images: work.images,
+      tag_name: tag.tag_name,
+      user_id: work.user_id
+    }
+  end
+
 end
